4fc3652784ac1cf96d7b39a2abaa65c4cd590e51
fl1pHover <bishu.hover@gmail.com>
2022-04-04 06:04:25 +0800

    responsive done

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,7 +3,7 @@ import './nav.css';
 import navLogo from '../../assets/Logos/knwmdzLogo.png';
 import { BiMenuAltRight } from 'react-icons/bi';
 window.addEventListener('scroll', function() {
-	var navbar = document.getElementById('nav');
+	var navbar = document.getElementById('nav__container');
 	navbar.classList.toggle('bg', window.scrollY > 0);
 });
 
@@ -14,73 +14,93 @@ window.addEventListener('scroll', function() {
 
 const Navbar = () => {
 	const [toggleMenu, setToggleMenu] = useState(false);
+	const [toggleOverlay, setToggleOverlay] = useState(false);
 
 	const toggleNav = () => {
 		setToggleMenu(!toggleMenu);
+		// setToggleOverlay(!toggleOverlay);
 	};
 	return (
 		<>
 			<section id="nav">
-				<div className="container">
-					<div className="nav__logo">
-						<a href="#header">
-							<img src={navLogo} alt="" />
-						</a>
-					</div>
-					<div className="nav__lists">
-						{/* {toggleMenu && ( */}
-						<ul className="lists">
-							<li>
-								<a href="#experience">Бидний тухай</a>
-							</li>
-							<li>
-								<a href="#event">Эвент</a>
-							</li>
-							<li>
-								<a href="#team">Гишүүд</a>
-							</li>
-							<li>
-								<a
-									href="#involve"
-									className="btn small__btn"
-								>
-									Нэгдэх
-								</a>
-							</li>
-						</ul>
-						{/* )} */}
-						{/* onClick={toggleNav} */}
-						<ul className="menu" onClick={toggleNav}>
-							<li>
-								<BiMenuAltRight className="menuIcon" />
-							</li>
-						</ul>
+				<div id="nav__container">
+					<div className="container">
+						<div className="nav__logo">
+							<a href="#header">
+								<img src={navLogo} alt="" />
+							</a>
+						</div>
+						<div className="nav__lists">
+							{/* {toggleMenu && ( */}
+							<ul className="lists">
+								<li>
+									<a href="#experience">
+										Бидний тухай
+									</a>
+								</li>
+								<li>
+									<a href="#event">Эвент</a>
+								</li>
+								<li>
+									<a href="#team">Гишүүд</a>
+								</li>
+								<li>
+									<a
+										href="#involve"
+										className="btn small__btn"
+									>
+										Нэгдэх
+									</a>
+								</li>
+							</ul>
+							{/* )} */}
+							{/* onClick={toggleNav} */}
+							<ul className="menu" onClick={toggleNav}>
+								<li>
+									<BiMenuAltRight className="menuIcon" />
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-				<div id="hidden">
-					{toggleMenu && (
-						<ul className="hidden__lists">
-							<li>
-								<a href="#experience">Бидний тухай</a>
-							</li>
-							<li>
-								<a href="#event">Эвент</a>
-							</li>
-							<li>
-								<a href="#team">Гишүүд</a>
-							</li>
-							<li>
-								<a
-									href="#involve"
-									className="btn small__btn"
-								>
-									Нэгдэх
-								</a>
-							</li>
-						</ul>
-					)}
+				<div id="hidden" className={toggleMenu ? 'close' : 'menu'}>
+					{/* {toggleMenu && ( */}
+					<ul className="hidden__lists">
+						<li>
+							<a href="#experience">Бидний тухай</a>
+						</li>
+						<li>
+							<a href="#event">Эвент</a>
+						</li>
+						<li>
+							<a href="#team">Гишүүд</a>
+						</li>
+						<li>
+							<a
+								href="#involve"
+								className="btn small__btn"
+							>
+								Нэгдэх
+							</a>
+						</li>
+					</ul>
+					{/* )} */}
+					{/* <div
+						className="overlay"
+						onClick={() => setToggleOverlay(!toggleOverlay)}
+					>
+						<span
+							className={toggleOverlay ? 'close' : 'menu'}
+						>
+							asdasasdasd
+						</span>
+					</div> */}
 				</div>
 			</section>
+			<div
+				className="overlay"
+				className={toggleMenu ? 'overlay' : 'lay'}
+			></div>
 		</>
 	);
 };
